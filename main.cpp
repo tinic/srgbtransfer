55f753172e78c72a2fa02031da76e16222069ff1
Tinic Uro <turo@adobe.com>
2019-11-11 14:48:13 -0800

    Avoid double promotion and optimize generic version.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,15 @@
 #include <chrono>
 
 static uint8_t sRGBTransfer(float a) {
-  if (a < 0.0031308f) {
-  	  return uint8_t(255.0f * std::clamp(a * 12.92f, 0.0f, 1.0f));
-  } else {
-	  return uint8_t(255.0f * std::clamp(pow(a, 1.0f/2.4f) * 1.055f - 0.055f, 0.0f, 1.0f));
-  }
+	if (a <= 0.0f) {
+		return 0;
+	} else if (a < 0.0031308f) {
+		return uint8_t(255.0f * std::clamp(a * 12.92f, 0.0f, 1.0f));
+	} else if ( a < 1.0f ) {
+		return uint8_t(255.0f * std::clamp(powf(a, 1.0f/2.4f) * 1.055f - 0.055f, 0.0f, 1.0f));
+	} else {
+		return 255;
+	}
 }
 
 static uint8_t sRGBTransferFast(float a) {
@@ -25,19 +29,17 @@ static uint8_t sRGBTransferFast(float a) {
 		} u = { a };
 		u.x[1] = (int16_t)((1.0f/2.40f) * float(u.x[1] - 0x3f7d) + 0x3f7d);
 		u.x[0] = 0;
-		return uint8_t((u.d * 1.055f - 0.055) * 255.0f);
+		return uint8_t((u.d * 1.055f - 0.055f) * 255.0f);
 	} else {
 		return 255;
 	}
 }
 
 int main() {
-	int32_t error = 0;
 	for(float i = -0.1f; i <= 1.1f; i += 1.0f/1024.0f) {
 		uint8_t a = sRGBTransfer(i);
 		uint8_t b = sRGBTransferFast(i);
-		error += abs(a-b);
-		printf("input: %f correct: %d fast: %d error: %d\n", i, a, b, abs(a-b));
+		printf("input: %f correct: %d fast: %d error: %d\n", double(i), a, b, abs(a-b));
 	}
 	
 	{	
